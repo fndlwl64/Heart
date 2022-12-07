87eefad66fdcc0de96a5a1cce3b93ae1f7578780
LeeBeomYong <soopwe2@naver.com>
2022-12-07 14:12:31 +0900

    xml create
    xml create

---
+++ src/main/java/com/heartpet/model/Mypage_SupportDTO.java
@@ -0,0 +1,13 @@
+package com.heartpet.model;
+
+import lombok.Data;
+
+@Data
+public class Mypage_SupportDTO {
+
+	private int support_no;
+	private int support_userno;
+	private int support_price;
+	private String support_date;
+	
+}
