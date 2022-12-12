c455539d3870f1079f8907776d7c3a52336c0f1c
YJ Nam <107021486+YJ-NAM@users.noreply.github.com>
2022-12-12 09:20:38 +0900

    Update heartpet_board.sql
    게시판 수정중

--- src/main/webapp/sql/heartpet_board.sql
+++ src/main/webapp/sql/heartpet_board.sql
@@ -10,7 +10,12 @@ create table heartpet_board (
     board_regdate date,
     board_update date,
     board_img1 varchar2(1000),
-    board_img2 varchar2(1000)
+    board_img2 varchar2(1000),
+    board_group number, // 부모글 group
+    board_level number, // 부모글 level + 1
+    board_category
+    board_answerNo number,
+    board_parentNo number   // 부모글 번호(부모글 board_no)
 );
 
 insert into heartpet_board values(1, '입양', 'user', 0, '게시판 제목 1', '게시판 1내용입니다', 'N', '1234', sysdate, null, null, null);
