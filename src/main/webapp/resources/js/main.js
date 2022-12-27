8addb4f4483c04a85584504fbf43cad031337966
YoonJi <holst15@naver.com>
2022-12-27 11:49:26 +0900

    main 수정중
    main 수정중

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
-        }
+        },
     });
     
     const cat_swiper = new Swiper(".catSwiper", {
@@ -36,28 +36,26 @@ $(document).ready(function() {
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
-        }
+        },
     });
 
     // reviewSwiper
     const review_swiper = new Swiper(".reviewSwiper", {
-        effect: "coverflow",
-        grabCursor: true,
+        slidesPerView: 3,
+        spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
-            delay: 3000,
+            delay: 10000,
             disableOnInteraction: false,
         },
-        slidesPerView: "auto",
-        coverflowEffect: {
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: false,
-        },
+        loop: true,
         pagination: {
             el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
     });
 
